ac8fc1517026dcf77af1778d64bd17037d069914
Renaldy Rizki Maulana <renaldy.rizki@gmail.com>
2024-08-11 17:04:59 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  // Declare a state variable to hold the input value
+  const [inputValue, setInputValue] = useState("");
+  const [reverse, setReverse] = useState(0); // State to hold the reverse result
+  const [difference, setDifference] = useState(0); // State to hold the difference result
+
+  // Function to handle input change
+  function handleInputOnChange(event) {
+    // Removes any leading zero and any non-digit character
+    const sanitizedValue = event.target.value.replace(/^0(?=\d)|[^0-9]/g, "");
+    // Update the inputValue
+    setInputValue(sanitizedValue);
+  }
+
+  // Function to handle the submit button click
+  function handleInputOnSubmit() {
+    let reverseNumber = getReverse(inputValue);
+    // Update the reverse result with the current input value
+    setReverse(reverseNumber);
+    setDifference(Math.abs(inputValue - reverseNumber));
+  }
+
+  function getReverse(stringNumber) {
+    // Reverse the string number
+    let reverseStr = stringNumber.split("").reverse().join("");
+    // Convert the result to a number
+    return parseInt(reverseStr, 10);
+  }
+
+  return (
+    <div className="App">
+      <div>
+        Number:{" "}
+        <input type="text" value={inputValue} onChange={handleInputOnChange} />
+        <button onClick={handleInputOnSubmit}>Submit</button>
+      </div>
+      <div>Reverse: {reverse}</div>
+      <div>Difference: {difference}</div>
+    </div>
+  );
+}
